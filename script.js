b3240b03006dfb42ac240fe70d0b423c1f08f79f
santyrevi <santyreyesvi@gmail.com>
2025-07-15 06:23:20 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
+
+// Texto que se mostrará
+const messageText = "Feliz graduación a la más bonita e inteligente diseñadora gráfica Te Quiero Mucho";
+
+// Elemento donde se insertará el texto
+const messageElement = document.getElementById("message");
+
+let index = 0;
+
+function typeWriter() {
+  if (index < messageText.length) {
+    messageElement.innerHTML += messageText.charAt(index);
+    index++;
+    setTimeout(typeWriter, 60); // Velocidad de escritura
+  }
+}
+
+window.onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+    typeWriter(); // Iniciar efecto de máquina de escribir
+  }, 1000);
+};
